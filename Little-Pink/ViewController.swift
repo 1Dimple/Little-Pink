1e413e9df11bcfb5961f5d791a37d32685a88d63
小林 <3272586351@qq.co>
2023-11-10 10:19:48 +0800

    做了UI2

--- Little-Pink/ViewController.swift
+++ Little-Pink/ViewController.swift
@@ -11,7 +11,8 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-      print("做了UI")
+        print("做了UI")
+        print("做了UI2")
     }
 
 
